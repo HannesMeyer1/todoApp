d8982339b5e12e0301ef6a33e62ac51a5cb7f930
HannesMeyer1 <44499739+HannesMeyer1@users.noreply.github.com>
2019-09-18 14:04:41 +0200

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TodoList'`, () => {
+  it(`should have as title 'hannes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TodoList');
+    expect(app.title).toEqual('hannes');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TodoList!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hannes!');
   });
 });
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Todo} from '../../model/Todo';
 
 @Component({
   selector: 'app-list',
@@ -7,17 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  newTodo: Todo;
   todos: Todo[] = [];
-  toggle(todo: Todo) {
-    todo.done = true;
-  }
-
-
-  constructor(public label: String) {
-    this.label
+  constructor() {
+    this.newTodo = new Todo('Bier');
+    this.newTodo = new Todo('Alkohol');
+    this.newTodo = new Todo('Softdrinks');
   }
 
   ngOnInit() {
   }
 
+  toggle(todo: Todo): void {
+    todo.done = !todo.done;
+  }
+
+  save(): void {
+    this.todos.push(this.newTodo);
+    this.newTodo = new Todo('');
+  }
+
+  delete(todo: Todo): void {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
+
+  search(input: any) {
+    this.todos.find(t => t.label === t.label);
+  }
 }
