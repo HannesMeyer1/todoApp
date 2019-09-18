ddc44dbf8c0daf108f5d43dc55f661cb6a02332a
HannesMeyer1 <44499739+HannesMeyer1@users.noreply.github.com>
2019-09-18 13:06:06 +0200

    Add files via upload

---
+++ src/app/app/components/list/list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Todo} from '../../model/Todo';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  newTodo: Todo;
+  todos: Todo[] = [];
+  constructor() {
+    this.newTodo = new Todo('');
+  }
+
+  ngOnInit() {
+  }
+
+  toggle(todo: Todo): void {
+    todo.done = !todo.done;
+  }
+
+  save(): void {
+    this.todos.push(this.newTodo);
+    this.newTodo = new Todo('');
+  }
+
+  delete(todo: Todo): void {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
+
+  search(input: any) {
+    this.todos.find(t => t.label === t.label);
+  }
+}
